c747d409c301d33494d1e8f58334e38005fad225
Shanoon Issaka <133187982+shanooni@users.noreply.github.com>
2025-11-03 02:12:11 +0000

    update main and requirements

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from dataloader import get_device, get_transforms, load_dataset
+from dataloader import get_device, get_transforms, load_dataset, load_dataset_with_limits
 from feature_extractor import load_resnet_model, extract_features, load_pretrained_model
 from helpers import save_features
 
 def main(data_dir, output_dir):
     device = get_device()
     transform = get_transforms()
-    dataset, dataloader = load_dataset(data_dir, transform)
-    model, model_name = load_pretrained_model(device=device, model_name="efficientnet_b0")
+    # dataset, dataloader = load_dataset(data_dir, transform)
+    dataset, dataloader = load_dataset_with_limits(data_dir, transform, max_samples=500, shuffle=True)
+    model, model_name = load_pretrained_model(device=device, model_name="efficientnet_b5")
 
     features, labels = extract_features(model, dataloader, device)
     print(f"Features shape: {features.shape}, Labels shape: {labels.shape}")
@@ -15,7 +16,7 @@ def main(data_dir, output_dir):
 
 if __name__ == "__main__":
     data_dir = "/Users/shanoonissaka/Documents/school/thesis-project/datasets/images/train"
-    main(data_dir, output_dir="./extracted_features")
+    main(data_dir, output_dir="./features")
 
     """
     To run different pretrained models, modify the model_name parameter in the main function call.
